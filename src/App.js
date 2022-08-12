82811ed4929de1daa26f12adb441ec6d66e2d0ac
sgujuluva <sanghee04@gmail.com>
2022-08-12 16:17:09 +0200

    routing path for home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 //react lib
-import {Routes, Route} from "react-router-dom"
+import {Routes, Route} from "react-router-dom";
+//components
+import Home from "../src/components/Home";
 //styles
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     <Routes>
+      <Route path="/" element={<Home/>}>
+      <Route path="/hotels" element={<HotelList/>}>
+      </Route>
+     </Routes>
     </div>
   );
 }
---
+++ src/components/HotelList/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HotelList() {
+  return (
+    <div>HotelList</div>
+  )
+}
+
+export default HotelList
