0065564e1b445915c2b5ceebb83737b6513ca45a
sgujuluva <sanghee04@gmail.com>
2022-09-17 19:13:56 +0200

    sign in created

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
     <div className="home-page">
          <Header />
          <div className="banner"></div>
-         <Signin/>
+       {/*  <Signin/> */}
         </div>
   )
 }
--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import {Link} from "react-router-dom";
 //images
 import Close from "../../images/close.png";
 //styles
@@ -10,6 +11,19 @@ function Signin() {
             <img src={Close} alt="" />
             <h2>CLOSE</h2>
         </div>
+        <div className="heading">
+            <h1>Your Account</h1>
+            <div className="accounts">
+                <h3 className="signin">Sign In</h3>
+                <h3 className="signup">Create an Account</h3>
+            </div>
+        </div>
+        <form >
+            <input type="email" name="email"  />
+            <input type="password" name="password"  />
+            <Link><h4>Forgotten your password?</h4></Link>
+            <button>LOGIN</button>
+        </form>
     </div>
   )
 }
