014aba86095982dfc8b285c290271a4ddcdfa2fb
sgujuluva <sanghee04@gmail.com>
2022-09-17 17:39:54 +0200

    language comp added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 //react lib
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 //components
 import Header from "./components/Header/Header";
 
 //styles
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     <Header/>
-      <h1>Hello</h1>
-       
-      <button>Reserve</button>
+      <Header />
     </div>
   );
 }
